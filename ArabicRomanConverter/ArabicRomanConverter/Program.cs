4ad37bee5f1faf443b69c447f0cfb6e1a71d6bfd
lchicu <lchicu@EN32193>
2018-10-05 18:58:15 +0300

    s

--- ArabicRomanConverter/ArabicRomanConverter/Program.cs
+++ ArabicRomanConverter/ArabicRomanConverter/Program.cs
@@ -10,27 +10,62 @@ namespace ArabicRomanConverter
     {
         static void Main(string[] args)
         {
+            string s= Console.ReadLine();
+            var a = RomanNumbers.C;
+                Console.WriteLine("a");
+
+
         }
     }
-    class ConvertRomanToArabic
+    public static class RomanArabicMap
     {
+        public static Dictionary<string, int> map = new Dictionary<string, int>()
+        {
+            { "I", 1 },
+            { "V", 5},
+            { "X", 10 },
+            { "L", 50 },
+            { "C", 100 },
+            { "D", 500 },
+            { "M", 1000 }
+        };
+    }
+    static class ConvertRomanToArabic
+    {
+        public static int Convert(this string s)
+        {
+            int result;
+            if (s == "" || s == null) result = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var arabic = RomanArabicMap.map[s[i].ToString()];
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if
+                }
+            }
+            
+            return result;
+            
+        }
+        
 
     }
-    class ConvertArabicToRoman
+    static class ConvertArabicToRoman
     {
 
     }
     
-    enum RomanNumbers
-    {
-        I =1,
-        V =5,
-        X=10,
-        L=50, 
-        C=100,
-        D =500,
-        M=1000
-    }
+    //enum RomanNumbers
+    //{
+    //    I = 1,
+    //    V = 5,
+    //    X = 10,
+    //    L = 50, 
+    //    C = 100,
+    //    D = 500,
+    //    M = 1000
+    //}
     
 
 }
