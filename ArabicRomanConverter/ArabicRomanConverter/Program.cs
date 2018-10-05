74c2c3ca8989d6802fd38091369dd71a0d526fe0
lchicu <lchicu@EN32193>
2018-10-05 18:03:24 +0300

    add new enum and classees

--- ArabicRomanConverter/ArabicRomanConverter/Program.cs
+++ ArabicRomanConverter/ArabicRomanConverter/Program.cs
@@ -12,4 +12,25 @@ namespace ArabicRomanConverter
         {
         }
     }
+    class ConvertRomanToArabic
+    {
+
+    }
+    class ConvertArabicToRoman
+    {
+
+    }
+    
+    enum RomanNumbers
+    {
+        I =1,
+        V =5,
+        X=10,
+        L=50, 
+        C=100,
+        D =500,
+        M=1000
+    }
+    
+
 }
