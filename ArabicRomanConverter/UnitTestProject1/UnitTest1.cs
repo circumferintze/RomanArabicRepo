3e62b138234127bb91a5fb1146c9a784d43dccce
Lilia Chicu <lilia.chicu@endava.com>
2018-10-07 13:20:40 +0300

    add test for Convert Method

---
+++ ArabicRomanConverter/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArabicRomanConverter;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ConvertTest()
+        {
+            int expected = 0;
+            var actual = ConvertRomanToArabic.Convert("");
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
